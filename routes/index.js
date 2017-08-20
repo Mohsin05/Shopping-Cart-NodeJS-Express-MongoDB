da300ed95cacb3166af56e8a913a3402ac844020
mohsin05 <mohsinyounas05@gmail.com>
2017-08-20 03:38:41 +0200

    Signup/SignIn/Dynamically Data Displayed From DataBase

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var Product= require('../models/product');
+
+
 /* GET home page. */
 
 //Read
 
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function (err,docs) {
+
+    var productChunks = [];
+    var chunkSize = 3;
+
+    for (var i=0; i< docs.length; i += chunkSize){
+
+        productChunks.push(docs.slice(i, i + chunkSize));
+
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks});
+
+  });
+});
+router.get('/signup', function(req, res, next) {
+
+  res.render('user/signup', { title: 'Signup'});
+
+
+});
+router.get('/signin', function(req, res, next) {
+
+  res.render('user/signin', { title: 'Signin'});
+
+
 });
 
 module.exports = router;
